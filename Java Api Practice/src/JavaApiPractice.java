b86879c0af007d2586cea824600563b23b23c3fc
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2019-08-30 10:19:26 -0600

    Finish Third String Commit

---
+++ Java Api Practice/src/JavaApiPractice.java
@@ -0,0 +1,17 @@
+
+public class JavaApiPractice
+	{
+
+		public static void main(String[] args)
+			{
+				String phraseOne = "Mullen High School";
+				String phaseTwo = "mullen";
+				String phaseThree = "mullen High school";
+
+				System.out.println(phraseOne.substring(0, 3));
+
+				System.out.println(phraseOne.substring(7, 11));
+				
+			}
+
+	}
