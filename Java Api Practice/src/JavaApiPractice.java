7598961d8b520eba1763296d08362e4f2dfe5556
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2019-09-04 12:05:05 -0600

    Finished Commit

--- Java Api Practice/src/JavaApiPractice.java
+++ Java Api Practice/src/JavaApiPractice.java
@@ -5,13 +5,29 @@ public class JavaApiPractice
 		public static void main(String[] args)
 			{
 				String phraseOne = "Mullen High School";
-				String phaseTwo = "mullen";
-				String phaseThree = "mullen High school";
+				String phraseTwo = "Hello";
+				String phraseThree = "Goodbye Everyone";
 
 				System.out.println(phraseOne.substring(0, 3));
 
 				System.out.println(phraseOne.substring(7, 11));
-				
+
+				System.out.println(phraseTwo.length());
+
+				System.out.println(phraseThree.codePointCount(0, 6));
+
+				System.out.println(phraseThree.codePointCount(0, 15));
+
+				System.out.println(phraseThree.concat(phraseTwo));
+
+				System.out.println(phraseThree.replace("e", "x"));
+
+				System.out.println(phraseOne.toUpperCase());
+
+				System.out.println(phraseOne.replaceAll(" ", ""));
+
+				System.out.println(phraseOne.compareTo(phraseThree));
+
 			}
 
 	}
